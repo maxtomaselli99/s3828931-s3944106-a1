f3efe1d4cf8ded7873d08639ff59f9c6b94cc6f7
maxtomaselli99 <maxtomaselli99@gmail.com>
2024-04-21 14:40:15 +1000

    new home page, pretty ugly, discount data stored in local storage

--- assignment_1/src/App.js
+++ assignment_1/src/App.js
@@ -3,18 +3,33 @@ import Header from './pages/Header';
 import Navbar from './pages/Navbar';
 import Content from './pages/Content';
 import Footer from './pages/Footer';
+import Sidebar from './pages/Sidebar';
+import { useState } from "react";
+
 
 function App() {
-  return (
+    const [BananaDiscount = 20, setBananaDiscount] = useState();
+    const [PotatoDiscount = 12, setPotatoDiscount] = useState();
+    const [CherryDiscount = 50, setCherryDiscount] = useState();
+    useState(() => {
+        localStorage.setItem('Banana', JSON.stringify(BananaDiscount));
+        localStorage.setItem('Potato', JSON.stringify(PotatoDiscount));
+        localStorage.setItem('Cherry', JSON.stringify(CherryDiscount));
+    }, [BananaDiscount]);
+
+    return (
       <div>
           <Header></Header>
           <Navbar></Navbar>
-          <div className="row">
+          <div className="grid mx-auto grid-cols-4 grod-flow-col">
               <Content></Content>
+              <Sidebar></Sidebar>
           </div>
           <Footer></Footer>
       </div>
   );
 }
+
+
 
 export default App;
--- assignment_1/src/pages/Sidebar.js
+++ assignment_1/src/pages/Sidebar.js
@@ -1,31 +1,19 @@
 import React from "react";
+import logo from '../logo.svg';
 
 const Sidebar = () => {
     return (
-        <div className="side">
-            <h2>Arcu bibendum</h2>
-            <h5>Sit amet mattis vulputate</h5>
-            <div className="fakeimg" style={{ height: 200 }}>
-                Image
+        <section class="px-10 py-10">
+            <div class="w-80 bg-white shadow-md">
+                <h1 class="text-2xl font-bold flex justify centre">Want to grow Veggies like us?</h1>
+                <p>1. Choose a sunny location</p>
+                <p>2. Use soil rich in organic matter</p>
+                <p>3. Water just the right amount</p>
+                <p>4. Pest Control</p>
+                <p>5. Don't Fertilise, use compost</p>
+                <p>6. Pair veggies to complement growth</p>
             </div>
-            <p>
-                Non blandit massa enim nec dui nunc mattis enim. Egestas tellus rutrum
-                tellus pellentesque eu tincidunt tortor aliquam nulla..
-            </p>
-            <h3>Massa enim</h3>
-            <p>Lorem ipsum dolor sit ame.</p>
-            <div className="fakeimg" style={{ height: 60 }}>
-                Image
-            </div>
-            <br />
-            <div className="fakeimg" style={{ height: 60 }}>
-                Image
-            </div>
-            <br />
-            <div className="fakeimg" style={{ height: 60 }}>
-                Image
-            </div>
-        </div>
+        </section>
     );
 };
 
